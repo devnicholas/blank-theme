29884db90ffc3283e183d0fe26f350376ae699f0
Nicholas Stefano <nicholas.stefanob@gmail.com>
2022-05-17 09:07:19 -0300

    feat: add helpers functions

--- inc/template-helpers.php
+++ inc/template-helpers.php
@@ -3,3 +3,17 @@ function format_price($price)
 {
 	return number_format($price, 2, ',', '.');
 }
+
+function image($path){
+	return get_template_directory_uri() . '/images/' . $path;
+}
+
+function the_theme_config($key)
+{
+	return the_field($key, 'option');
+}
+
+function get_page_url($page)
+{
+	return get_permalink(get_page_by_title($page));
+}
