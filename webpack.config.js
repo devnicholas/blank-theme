39f9cf2933bb6e0448e92c5d4bcf2a72220462c2
Nicholas Stefano <nicholas.stefanob@gmail.com>
2025-03-05 17:21:25 -0300

    feat: add tailwindcss to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,41 @@
 var glob = require("glob");
 var path = require("path");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     scripts: glob.sync("./resources/js/*.js"),
+    styles: "./resources/css/style.css", // Adiciona o Tailwind na entrada
   },
   output: {
     path: path.join(__dirname, "./static/dist"),
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("@tailwindcss/postcss"),
+                  require("autoprefixer"),
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
