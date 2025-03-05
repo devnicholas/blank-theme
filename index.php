250d21644627d114b5104fc646b52ab8961c001c
Nicholas Stefano <nicholas.stefanob@gmail.com>
2025-03-05 15:05:20 -0300

    feat: updating to tailwindcss 4.0

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
-<div class="welcome-wrapper">
-    <div class="logo">
-        <div></div>
+<div class="container h-screen flex flex-col py-28 justify-between items-center">
+    <div class="border-2 border-primary p-4">
+        <div class="bg-primary w-20 h-20"></div>
     </div>
-    <h1 class="theme-title">BLANK</h1>
-    <h2 class="theme-subtitle">Wordpress Theme</h2>
-    <div class="actions">
-        <a href="https://github.com/devnicholas/blank-theme" target="_blank" rel="noopener noreferrer">
+    <h1 class="font-bold text-6xl text-primary">BLANK</h1>
+    <h2 class="text-secondary text-3xl">Wordpress Theme</h2>
+    <div class="text-secondary font-light flex gap-4">
+        <a href="https://github.com/devnicholas/blank-theme" target="_blank" rel="noopener noreferrer" class="underline">
             GITHUB
         </a>
     </div>
     <div class="start-line">
         <p>
-            Go to <code>/index.php</code> file to start editing your theme.
+            Go to <code class="bg-gray-200 p-1 rounded-sm">/index.php</code> file to start editing your theme.
         </p>
     </div>
 </div>
